088559322d7ab3fa9040ed18b1d22b160fa956ad
Aldo <aldomhm01@gmail.com>
2023-06-02 14:20:47 -0400

    init functions

---
+++ NeuralNet.h
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <math.h>
+
+struct matrix{
+    double **arr;
+    int row;
+    int col;
+};
+
+struct vector{
+    double *arr;
+    int len;
+};
+
+struct layer{
+    struct matrix *random_weights;
+    struct vector *bias;
+    int output;
+    int input;
+};
+
+struct neuralnet{
+    struct layer *layers;
+    struct layer *input;
+    struct layer *output;
+    int len;
+};
+
+void init_layer(struct layer, int, int, struct matrix, struct vector);
+
+void init_neuralnet(int, struct layer, struct layer, struct layer, struct neuralnet);
+
+void init_random(struct matrix);
